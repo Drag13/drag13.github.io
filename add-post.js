15f18693611613b3eada216208d4917b907b5816
Vitalii Drag <ruban.vitaliy@gmail.com>
2020-06-16 18:57:36 +0300

    Styling

--- add-post.js
+++ add-post.js
@@ -2,24 +2,25 @@ const { mkdirSync, writeFileSync } = require("fs");
 const { join } = require("path");
 const { cwd } = require("process");
 
-console.log(process.argv);
-
-const [postName] = process.argv.slice(2);
 const indexPugTemplate = (title) =>
-`extends ./../../_layout.pug
+  `extends ./../../_layout.pug
 
 block title
   title ${title}
 
 block content
-  include:markdown-it content.md
+  article
+    include:markdown-it content.md
 `;
 
-const title = postName
-  .split("-")
-  .map((x) => x.replace(x[0], x[0].toUpperCase()))
-  .join(" ");
+const generateTitleName = (postName) =>
+  postName
+    .split("-")
+    .map((x) => x.replace(x[0], x[0].toUpperCase()))
+    .join(" ");
 
+const [postName, titleName] = process.argv.slice(2);
+const title = titleName || generateTitleName(postName);
 const folder = join(cwd(), `./src/posts/${postName}`);
 
 mkdirSync(folder);
